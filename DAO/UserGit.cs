637338932c66e3804399e4e64a999059dd9766e3
Rafael Tavares Andrade Toledo <rafaeltavaresandrade@gmail.com>
2018-08-30 13:10:56 -0300

    update

---
+++ DAO/UserGit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace DAO
+{
+    public class UserGit
+    {
+        public void ObterDetalhesUsuarioGithub()
+        {
+
+        }
+
+        public void ObterMeusRepositorios()
+        {
+
+        }
+
+    }
+}
